ad5c74df0c91453e7473a016fb935f63062af522
BadTudou <badtudou@gmail.com>
2016-11-02 22:56:56 +0800

    1.add previous commit lost something (添加上个提交中忘记的部分)

---
+++ public/javascripts/src/components/MessageBox.vue
@@ -0,0 +1,50 @@
+<template>
+<div class=" col-lg-12" style="background-color: #123">
+sfsdfsdf
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+          <span class="sr-only">Close</span>
+        </button>
+        <h4 class="modal-title">消息框</h4>
+      </div>
+      <div class="modal-body">
+        <p><strong>{{msgTextType}}!</strong>{{msgText}}</p>
+      </div>
+	</div>
+  </div>
+</div>
+</template>
+<script>
+// export default {
+//  	data () {
+//  		return {
+//  			tips: {success:{type:'alert alert-success',	text:'成功'},
+//  					err: 	 {type:'alert alert-danger',	text:'失败'},
+//  					warn:  {type:'alert alert-warning',   text:'警告'},
+//  					info:  {type:'alert alert-info',	 	text:'信息'}
+//  				 }
+//  		}
+//  	},
+//  	methods: {
+//  		msgType:function(){
+//  			return this.$store.state.msgType;
+//  		},
+//       msgClassType: function() {
+//       	return this.tips[this.msgType].type;
+//       },
+//       msgTextType: function(){
+//       	return this.tips[this.msgType].text;
+//       },
+//       msgText:function() {
+//       	return this.$store.state.msgText;
+//       }, 
+//       isShow:function() {
+//       	//return this.$store.state.msgboxIsShow;
+//       	return true;
+//       }
+//  	}
+//  }	
+</script>
--- public/javascripts/src/index.js
+++ public/javascripts/src/index.js
@@ -1,18 +1,21 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
-import store from './vuex/store';
+import Vue          from 'vue';
+import Vuex         from 'vuex';
+import store        from './vuex/store';
 //import App from './components/App.vue';
-import Logo 		from './components/Logo.vue';
-import Searchbox 	from './components/SearchBox.vue';
+import Logo 		    from './components/Logo.vue';
+import Searchbox  	from './components/SearchBox.vue';
 import Navigation 	from './components/Navigation.vue';
-import Location 	from './components/Location.vue';
-import Ad 			from './components/AD.vue';
+import Location 	  from './components/Location.vue';
+import Ad 			    from './components/AD.vue';
 import Articlelist 	from './components/ArticleCardList.vue';
 import Featurelist 	from './components/FeatureCardList.vue';
+import Artiiframe   from './components/ArticleIframe.vue';
+import Discusslist  from './components/DiscussList.vue';
 import Pagination 	from './components/Pagination.vue';
-import Personal 	from './components/Personal.vue';
-import Contact 		from './components/ContactMe.vue';
-import About 		from './components/AboutMe.vue';
+import Personal 	  from './components/Personal.vue';
+import Contact 		  from './components/ContactMe.vue';
+import About 		    from './components/AboutMe.vue';
+import Messagebox   from './components/MessageBox.vue';
 import Footerbar 		from './components/Footer.vue';
 
 //Vue.use(Vuex);
@@ -20,8 +23,9 @@ var vm = new Vue({
   store,
   el: '#app',
   components: { 
-  	Logo, Searchbox, Navigation, Location, Articlelist, Featurelist, Pagination, Personal, 
-  	Contact, About, Footerbar, Ad
+  	Logo, Searchbox, Navigation, Location, Articlelist, 
+    Featurelist, Pagination, Personal, Contact, About,
+    Footerbar, Ad, Artiiframe, Discusslist, Messagebox
   },
   methods:{
   	isArticleShow:function(){
